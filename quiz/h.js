c512df2b9366abd509e7ed93b3272779c634c589
krishnaKalyan553 <75462502+krishnaKalyan553@users.noreply.github.com>
2023-04-20 21:38:32 +0530

    Delete quiz directory

--- quiz/h.js
+++
@@ -1,5 +0,0 @@
-const s=JSON.parse(localStorage.getItem('highscores'))
-const l = document.getElementById("list");
-l.innerHTML = s.map(score=>{
-    return `<li class="high-scores">${score.nam}-${score.score}</li>`
-}).join(" ")
